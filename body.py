8224fa9539cc814382669479a3f467dabe83e9e6
lehjke <llesnichiy@ya.ru>
2026-02-03 17:58:47 +0300

    Add building-type copy/run workflow
    Refactor main to accept a buildingtype parameter and delegate copy-and-run behavior to makecopiesandrun. Introduce makecopiesandrun which creates per-building-type file copies, adjusts handling capacity, and invokes building-specific automation (resedence and office). Add resedence and office helper functions to automate Elevate runs and file selection, and add a modify_buildingtype stub (prints the intended change; XML update commented out). Remove the previous inline copy-and-launch logic from main and keep get_area usage. Includes basic error handling for unknown building types.

--- body.py
+++ body.py
@@ -14,41 +14,16 @@ from xml.dom import minidom
 
 import csv
 
-def main(n_copy, path) -> None:
+def main(n_copy, path, buildingtype) -> None:
     #n_copy=3
     #path=r'C:\Users\Pavel\Desktop\elevate_in\TEST\K21'
 
-    file = os.listdir(path)
     n_copy = int(n_copy)
-
-    for i in range(2, n_copy + 1):
-        if i < 10:
-            shutil.copyfile(path + '\\' + file[0], path + '\\' + file[0][:-6] + str(i) + '.elvx')
-        else:
-            shutil.copyfile(path + '\\' + file[0], path + '\\' + file[0][:-7] + str(i) + '.elvx')
-       
-        file = os.listdir(path)
-        modify_handling_capacity(path + '\\' + file[i-1], i)
-    
     get_area(path)
+    makecopiesandrun(buildingtype, path, n_copy) 
 
-    os.startfile(r'C:\Program Files (x86)\Elevate 9\Elevate.exe')
-    time.sleep(3.0)
-    py_win_keyboard_layout.change_foreground_window_keyboard_layout(0x04090409)
-    keyboard.press_and_release('alt + a')
-    keyboard.press_and_release('down arrow')
-    keyboard.press_and_release('down arrow')
-    keyboard.press_and_release('down arrow')
-    keyboard.press_and_release('down arrow')
-    keyboard.press_and_release('down arrow')
-    keyboard.press_and_release('enter')
-    time.sleep(0.5)
-    keyboard.press_and_release('tab')
-    keyboard.press_and_release('tab')
-    keyboard.press_and_release('tab')
-    time.sleep(0.5)
-    keyboard.press_and_release('ctrl + v')
-    keyboard.press_and_release('enter')
+    
+
 
 def print_repot(path) -> None:
         
@@ -98,6 +73,23 @@ def modify_handling_capacity(xml_file, new_capacity):
     with open(xml_file, 'w', encoding='utf-8') as f:
         f.write(pretty_xml)
 
+def modify_buildingtype(xml_file, buildingtype) -> None:
+    # tree = ET.parse(xml_file)
+    # root = tree.getroot()
+
+    # data_config = root.find('.//ElevatorData/Advanced/Configuration')
+    # if data_config is not None:
+    #     building_type = data_config.find('BuildingType')
+    #     if building_type is not None:
+    #         building_type.text = buildingtype
+
+    # xml_str = ET.tostring(root, encoding='utf-8')
+    # pretty_xml = minidom.parseString(xml_str).toprettyxml(indent="  ")
+    
+    # with open(xml_file, 'w', encoding='utf-8') as f:
+    #     f.write(pretty_xml)
+    print('Modifying building type to:', buildingtype)
+
 def get_area(path) -> None:
 
     xml_file = path + '\\' + os.listdir(path)[0]
@@ -129,5 +121,96 @@ def get_area(path) -> None:
             writer.writeheader()
             writer.writerows(data)
 
-if __name__ == '__main__':
-    main()
+def resedence(path) -> None:
+    os.startfile(r'C:\Program Files (x86)\Elevate 9\Elevate.exe')
+    time.sleep(3.0)
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(0x04090409)
+    keyboard.press_and_release('alt + a')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('enter')
+    time.sleep(0.5)
+    keyboard.press_and_release('tab')
+    keyboard.press_and_release('tab')
+    keyboard.press_and_release('tab')
+    time.sleep(0.5)
+    #keyboard.press_and_release('ctrl + v')
+    keyboard.write(path)
+    keyboard.press_and_release('enter')
+
+def office(path) -> None:
+    os.startfile(r'C:\Program Files (x86)\Elevate 9\Elevate.exe')
+    time.sleep(3.0)
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(0x04090409)
+    keyboard.press_and_release('alt + a')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('enter')
+    time.sleep(0.5)
+    keyboard.press_and_release('tab')
+    keyboard.press_and_release('tab')
+    keyboard.press_and_release('tab')
+    time.sleep(0.5)
+    #keyboard.press_and_release('ctrl + v')
+    keyboard.write(path + '\\' + 'morning')
+    keyboard.press_and_release('enter')
+    os.startfile(r'C:\Program Files (x86)\Elevate 9\Elevate.exe')
+    time.sleep(3.0)
+    py_win_keyboard_layout.change_foreground_window_keyboard_layout(0x04090409)
+    keyboard.press_and_release('alt + a')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('down arrow')
+    keyboard.press_and_release('enter')
+    time.sleep(0.5)
+    keyboard.press_and_release('tab')
+    keyboard.press_and_release('tab')
+    keyboard.press_and_release('tab')
+    time.sleep(0.5)
+    #keyboard.press_and_release('ctrl + v')
+    keyboard.write(path + '\\' + 'lunch')
+    keyboard.press_and_release('enter')
+
+def makecopiesandrun(buildingtype, path, n_copy) -> None:
+    file = os.listdir(path)
+    if buildingtype == 'resedence':
+        for i in range(2, n_copy + 1):
+            if i < 10:
+                shutil.copyfile(path + '\\' + file[0], path + '\\' + file[0][:-6] + str(i) + '.elvx')
+            else:
+                shutil.copyfile(path + '\\' + file[0], path + '\\' + file[0][:-7] + str(i) + '.elvx')
+       
+            file = os.listdir(path)
+            modify_handling_capacity(path + '\\' + file[i-1], i)
+            modify_buildingtype(path + '\\' + file[i-1],buildingtype)  
+        resedence(path)
+    elif buildingtype == 'office':
+        morningpath = path + '\\' + 'morning'
+        lunchpath = path + '\\' + 'lunch'
+        os.makedirs(morningpath)
+        os.makedirs(lunchpath)
+        shutil.copyfile(path + '\\' + file[0], morningpath + '\\' + file[0])
+        shutil.copyfile(path + '\\' + file[0], lunchpath + '\\' + file[0])
+        for i in range(2, n_copy + 1):
+            if i < 10:
+                shutil.copyfile(morningpath + '\\' + file[0], morningpath + '\\' + file[0][:-6] + str(i) + '.elvx')
+            else:
+                shutil.copyfile(morningpath + '\\' + file[0], morningpath + '\\' + file[0][:-7] + str(i) + '.elvx')
+            file = os.listdir(morningpath)
+            modify_handling_capacity(morningpath + '\\' + file[i-1], i)
+     
+            if i < 10:
+                shutil.copyfile(lunchpath + '\\' + file[0], lunchpath + '\\' + file[0][:-6] + str(i) + '.elvx')
+            else:
+                shutil.copyfile(lunchpath + '\\' + file[0], lunchpath + '\\' + file[0][:-7] + str(i) + '.elvx')
+            file = os.listdir(lunchpath)
+            modify_handling_capacity(lunchpath + '\\' + file[i-1], i) 
+            modify_buildingtype(lunchpath + '\\' + file[i-1],buildingtype)  
+        office(path) 
+    else:
+        print('Unknown building type')
+
